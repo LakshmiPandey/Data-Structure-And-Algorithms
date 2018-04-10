7bbb9260a7a57bc50ac09df2db70d63d7522de27
Developer_Aarna <35351079+aarna97pandey@users.noreply.github.com>
2018-04-10 22:56:42 +0530

    Update BinarySearchRecursive.cpp

--- Searching/BinarySearchRecursive.cpp
+++ Searching/BinarySearchRecursive.cpp
@@ -2,6 +2,8 @@
 
 #include<iostream>
 using namespace std;
+
+/* Function declaration*/
 int binary_search(int arr[], int b, int l, int ele);
 int main()
 {
@@ -43,49 +45,5 @@ int binary_search(arr[], int b, int l, int ele)
         return (binarySearch(arr, mid+1, r, x));
    }
   return(-1);
-//...................Recursion method ......................
-
-#include<iostream>
-using namespace std;
-int binary_search(int arr[], int b, int l, int ele);
-int main()
-{
-	int n=0 , ele, pos;
-	cout<<"\n Enter array size\n";
-	cin>>n;
-	int arr[n];
-	cout<<"\n Enter array elements\n";
-	for(int i=0;i<n;i+)
-	{
-	   cin>>arr[i];
-	}
-	cout<<"\n Enter the element to be searched\n";
-	cin>>ele;
-
-	/*Function calling binary search*/
-	pos = binary_search(arr ,n , 0 ,ele);
-
-	/* printing the postion of the elemnet found*/
-	if(pos == -1)
-	cout<<"\n Element not found the the given array\n";
-	else 
-	cout<<"\n Element is found at the position "<<pos<<" in the array";
-
-	return 0;
-}
-
-
-/* Defining binary search function */
-int binary_search(arr[], int b, int l, int ele)
-{
-	if (l >= b)
-   {
-        int mid = b + (l - b)/2;
-        if (arr[mid] == ele)  
-            return mid;
-        if (arr[mid] > x) 
-            return (binarySearch(arr, l, mid-1, x));
-        return (binarySearch(arr, mid+1, r, x));
-   }
-  return(-1);
-}
+}	
+	
